affcb42734459b5f0d6b3282f11ae62eae0ba46b
MitsuyaT <lautyrodriguez25@gmail.com>
2024-07-13 22:06:48 -0300

    Diseño de card

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+    borderRadius: {
+      md: "1.5rem",
+      lg: "3.5rem",
+    },
+  },
+  plugins: [],
+};
